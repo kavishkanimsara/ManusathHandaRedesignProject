8f1de9bcc4da875e571e490da16f2b15f7d7b7c4
Ramosh99 <einfetch@gmail.com>
2024-02-21 17:56:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Category from "./components/Category";
 import Hero from "./components/Hero";
+import Rec from "./pages/Rec";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 <Routes>
             <Route path="/category" element={<Category></Category>}></Route>
             <Route path="/" element={<Hero></Hero>}></Route>
+            <Route path="/rec" element={<Rec></Rec>}></Route>
           </Routes>
        {/* </BrowserRouter> */}
     </>
