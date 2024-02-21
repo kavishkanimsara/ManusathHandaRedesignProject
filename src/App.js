3160614a7648368383ec134a678cc1b9872119fa
Ramosh99 <einfetch@gmail.com>
2024-02-21 10:28:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Category from "./components/Category";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-       
+       {/* <BrowserRouter> */}
+<Routes>
+            <Route path="/category" element={<Category></Category>}></Route>
+            <Route path="/" element={<Hero></Hero>}></Route>
+          </Routes>
+       {/* </BrowserRouter> */}
     </>
   );
 }
