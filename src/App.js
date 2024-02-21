fd2f03d2602fe5ec1c35eb43b216cb0636579a02
Ramosh99 <einfetch@gmail.com>
2024-02-21 23:22:12 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Category from "./components/Category";
 import Hero from "./components/Hero";
-import Rec from "./pages/Rec";
 import LandingPage from "./pages/LandingPage";
 
 
@@ -9,13 +8,10 @@ function App() {
   return (
     <>
 
-       {/* <BrowserRouter> */}
-<Routes>
+          <Routes>
             <Route path="/category" element={<Category></Category>}></Route>
             <Route path="/" element={<Hero></Hero>}></Route>
-            <Route path="/rec" element={<Rec></Rec>}></Route>
           </Routes>
-       {/* </BrowserRouter> */}
     </>
   );
 }
