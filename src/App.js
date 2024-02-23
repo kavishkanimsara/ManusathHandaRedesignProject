412c55467263627cea1669ff24be40c6722f05b0
Ramosh99 <einfetch@gmail.com>
2024-02-23 14:37:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Page from "./pages/Page";
-
+import Hero from "./components/Hero";
+import Category from "./components/Categories";
 
 function App() {
   return (
     <>
 
           <Routes>
-            <Route path="/category" element={<Category></Category>}></Route>
             <Route path="/" element={<Hero></Hero>}></Route>
             <Route path="/new" element={<Page></Page>}></Route>
           </Routes>
